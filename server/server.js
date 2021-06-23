0e02ee8e78c1fedf754d5418ba46066ba821bfff
sebastian <sebastian bowen>
2021-06-23 13:02:24 -0600

    server turns on and runs at port 3001, As well as basic server side file structure, with npm packages installed

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+//const { ApolloServer } = require('apollo-server-express');
+//const path = require('path');
+//const { authMiddleware } = require('./utils/authorize')
+const { typeDefs, resolvers } = require('./schemas')
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+/*const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware
+})*/
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on the greatest port! ${PORT}!!!`);
+    })
+})
