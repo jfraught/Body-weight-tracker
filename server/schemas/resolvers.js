362d41ab375adf9550088b97fcea0eeca7760e74
sebastian <sebastian bowen>
2021-06-24 19:29:25 -0600

    class commit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,18 @@
-const {User, /*Profile, DayLog*/} = require('../models');
+const {User, Profile, DayLog} = require('../models');
 const { AuthenticationError} = require('apollo-server-express');
 const { signToken } = require('../utils/authorize');
 
 const resolvers = {
   Query: {
+      user: async (parent, args, context) => {
+          if (context.user) {
+              const userData = await User.findOne({_id: context.user._id})
+              .select('-__v -password')
+              
+              return userData
+          }
+          throw new AuthenticationError('not logged in');
+    },
 
   },  
   // mutation logic
