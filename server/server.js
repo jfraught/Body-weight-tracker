dd524ddfb9fa74b918e131513c4d915604cb16d4
sebastian <sebastian bowen>
2021-07-04 17:38:52 -0600

    manual build

--- server/server.js
+++ server/server.js
@@ -36,6 +36,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
   
+  app.get('*',(req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
   
 // port connection
 db.once('open', () => {
