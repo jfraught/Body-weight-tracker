9e61ec0db0ecb7ebc462f06454fccd94b9957143
sebastian <sebastian bowen>
2021-07-04 16:42:31 -0600

    apollo server fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-const { ApolloServer } = require('apollo-server-express');
+
 const express = require('express');
 
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/authorize');
 require('dotenv').config();
 // schema files
@@ -34,9 +35,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
   
-  app.get('*',(req, res) => {
+  /*app.get('*',(req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+  });*/
 
 // port connection
 db.once('open', () => {
